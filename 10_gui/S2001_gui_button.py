75fd55b471c157f0de06c84e2762f589adbea3d1
Gene <46876531+GreentheNinja@users.noreply.github.com>
2025-05-13 13:13:26 +0200

    What am I doing with my life

--- 10_gui/S2001_gui_button.py
+++ 10_gui/S2001_gui_button.py
@@ -11,14 +11,48 @@ import tkinter as tk  # import the GUI library
 # Define the main window, its title text and its size
 # These first 3 lines are typically roughly the same in every tkinter GUI
 main_window = tk.Tk()  # create the main window
-main_window.title('my first GUI')  # text shown in the top window bar
+main_window.title("A GUI Most Foul")  # text shown in the top window bar
 main_window.geometry("500x500")  # window size
 
 # Create a button
 # You can find all possible options of tk.Button() in the following documentations:
 # https://tkdocs.com/shipman/button.html
 # https://www.tutorialspoint.com/python/tk_button.htm#
-button_1 = tk.Button(main_window, text="Click me, I am a button")
+button_1 = tk.Button(
+    main_window,
+    text = "My",
+    anchor = "center"
+)
+button_2 = tk.Button(
+    main_window,
+    text = "compass",
+    anchor = "center"
+)
+button_3 = tk.Button(
+    main_window,
+    text = "was",
+    anchor = "center"
+)
+button_4 = tk.Button(
+    main_window,
+    text = "swallowed",
+    anchor = "center"
+)
+button_5 = tk.Button(
+    main_window,
+    text = "by",
+    anchor = "center"
+)
+button_6 = tk.Button(
+    main_window,
+    text = "the",
+    anchor = "center"
+)
+button_7 = tk.Button(
+    main_window,
+    text = "sea",
+    anchor = "center"
+)
 
 # Define where the button is positioned
 # Watch this video from 1:48 till 7:03 to understand positioning in tkinter:
@@ -26,7 +60,13 @@ button_1 = tk.Button(main_window, text="Click me, I am a button")
 # You can find all possible options of grid() in the following documentations:
 # https://tkdocs.com/shipman/grid.html
 # https://www.tutorialspoint.com/python/tk_grid.htm
-button_1.grid(row=0, column=1)
+button_1.grid(row = 0, column = 2, sticky = tk.E)
+button_2.grid(row = 1, column = 1, sticky = tk.W)
+button_3.grid(row = 2, column = 0, sticky = tk.E)
+button_4.grid(row = 3, column = 1, ipadx = 10, ipady = 10)
+button_5.grid(row = 4, column = 2, ipadx = 50, ipady = 20)
+button_6.grid(row = 5, column = 3, ipadx = 50, ipady = 20)
+button_7.grid(row = 6, column = 2, ipadx = 50, ipady = 20)
 
 # main program
 if __name__ == "__main__":  # Executed only when this file is run.
--- 10_gui/S2040_gui_exercise4.py
+++ 10_gui/S2040_gui_exercise4.py
@@ -22,3 +22,36 @@ Funktionalitet:
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
+
+import tkinter as tk
+
+def validate_id(message: str = "") -> bool:
+    if message == "":
+        return True
+    if not message.isnumeric():
+        return False
+    return len(message) <= 8
+
+window = tk.Tk()
+window.title("Hello GUIs")
+window.geometry("200x150")
+
+container = tk.LabelFrame(window, text = "Container")
+container.pack(expand = True, fill = "both", padx = 10, pady = 5)
+container.grid_rowconfigure(0, weight = 4)
+# container.grid_rowconfigure(1, weight = 1)
+container.grid_rowconfigure(2, weight = 6)
+container.grid_columnconfigure(0, weight = 1)
+
+id_label = tk.Label(container, text = "ID", justify = "center", width = 0)
+id_label.grid(row = 0, column = 0, sticky = tk.S)
+
+id_field = tk.Entry(container, width = 10, justify = "center", validate = "key", validatecommand = (window.register(validate_id), "%P"))
+id_field.grid(row = 1, column = 0, pady = 5)
+
+submit_button = tk.Button(container, text = "Submit", command = window.destroy)
+submit_button.grid(row = 2, column = 0, sticky = tk.N)
+
+
+if __name__ == '__main__':
+    window.mainloop()
